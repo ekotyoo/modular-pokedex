82249e9981c8a8d16e2c4549fd1520b7f4c97419
ekotyoo <ekeprasetyo@gmail.com>
2022-07-19 19:48:49 +0700

    Add local data source check in PokemonRepository

--- core/src/main/java/com/ekotyoo/core/data/repository/PokemonRepository.kt
+++ core/src/main/java/com/ekotyoo/core/data/repository/PokemonRepository.kt
@@ -8,7 +8,6 @@ import com.ekotyoo.core.domain.model.Pokemon
 import com.ekotyoo.core.domain.model.PokemonDetail
 import com.ekotyoo.core.domain.repository.IPokemonRepository
 import com.ekotyoo.core.utils.DataMapper
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.*
 import javax.inject.Inject
 import javax.inject.Singleton
@@ -20,62 +19,79 @@ class PokemonRepository @Inject constructor(
 ) : IPokemonRepository {
     override fun getPokemons(): Flow<Resource<List<Pokemon>>> = flow {
         emit(Resource.Loading())
-        when (val response = pokemonRemoteDataSource.getPokemons().first()) {
-            is ApiResponse.Success -> {
-                pokemonLocalDataSource.insertPokemons(DataMapper.mapResponsesToPokemonEntities(
-                    response.data))
-                val pokemons =
-                    DataMapper.mapPokemonEntitiesToDomains(pokemonLocalDataSource.getPokemons()
-                        .first())
-                emit(Resource.Success(pokemons))
+        if (pokemonLocalDataSource.getPokemons().first().isEmpty()) {
+            when (val response = pokemonRemoteDataSource.getPokemons().first()) {
+                is ApiResponse.Success -> {
+                    pokemonLocalDataSource.insertPokemons(DataMapper.mapResponsesToPokemonEntities(
+                        response.data))
+                    val pokemons =
+                        DataMapper.mapPokemonEntitiesToDomains(pokemonLocalDataSource.getPokemons()
+                            .first())
+                    emit(Resource.Success(pokemons))
+                }
+                is ApiResponse.Empty -> {
+                    val pokemons =
+                        DataMapper.mapPokemonEntitiesToDomains(pokemonLocalDataSource.getPokemons()
+                            .first())
+                    emit(Resource.Success(pokemons))
+                }
+                is ApiResponse.Error -> {
+                    emit(Resource.Error(response.errorMessage))
+                }
             }
-            is ApiResponse.Empty -> {
-                val pokemons =
-                    DataMapper.mapPokemonEntitiesToDomains(pokemonLocalDataSource.getPokemons()
-                        .first())
-                emit(Resource.Success(pokemons))
-            }
-            is ApiResponse.Error -> {
-                emit(Resource.Error(response.errorMessage))
-            }
+        } else {
+            emitAll(
+                pokemonLocalDataSource.getPokemons().map {
+                    Resource.Success(DataMapper.mapPokemonEntitiesToDomains(it))
+                }
+            )
         }
-    }.flowOn(Dispatchers.IO)
+    }
 
     override fun getPokemonDetail(name: String): Flow<Resource<PokemonDetail?>> = flow {
         emit(Resource.Loading())
-
-        when (val response = pokemonRemoteDataSource.getPokemonDetail(name).first()) {
-            is ApiResponse.Success -> {
-                pokemonLocalDataSource.insertPokemonDetail(
-                    DataMapper.mapPokemonDetailResponseToEntity(response.data)
-                )
-                emitAll(
-                    pokemonLocalDataSource.getPokemonDetail(name).map {
-                        Resource.Success(it?.let { pokemonDetail ->
-                            DataMapper.mapPokemonDetailEntityToDomain(pokemonDetail)
-                        })
-                    }
-                )
+        if (pokemonLocalDataSource.getPokemonDetail(name).first() == null) {
+            when (val response = pokemonRemoteDataSource.getPokemonDetail(name).first()) {
+                is ApiResponse.Success -> {
+                    pokemonLocalDataSource.insertPokemonDetail(
+                        DataMapper.mapPokemonDetailResponseToEntity(response.data)
+                    )
+                    emitAll(
+                        pokemonLocalDataSource.getPokemonDetail(name).map {
+                            Resource.Success(it?.let { pokemonDetail ->
+                                DataMapper.mapPokemonDetailEntityToDomain(pokemonDetail)
+                            })
+                        }
+                    )
+                }
+                is ApiResponse.Empty -> {
+                    emitAll(
+                        pokemonLocalDataSource.getPokemonDetail(name).map {
+                            Resource.Success(it?.let { pokemonDetail ->
+                                DataMapper.mapPokemonDetailEntityToDomain(pokemonDetail)
+                            })
+                        }
+                    )
+                }
+                is ApiResponse.Error -> {
+                    emit(Resource.Error(response.errorMessage))
+                }
             }
-            is ApiResponse.Empty -> {
-                emitAll(
-                    pokemonLocalDataSource.getPokemonDetail(name).map {
-                        Resource.Success(it?.let { pokemonDetail ->
-                            DataMapper.mapPokemonDetailEntityToDomain(pokemonDetail)
-                        })
-                    }
-                )
-            }
-            is ApiResponse.Error -> {
-                emit(Resource.Error(response.errorMessage))
-            }
+        } else {
+            emitAll(
+                pokemonLocalDataSource.getPokemonDetail(name).map {
+                    Resource.Success(it?.let { pokemonDetail ->
+                        DataMapper.mapPokemonDetailEntityToDomain(pokemonDetail)
+                    })
+                }
+            )
         }
-    }.flowOn(Dispatchers.IO)
+    }
 
     override fun getFavoritePokemons(): Flow<List<Pokemon>> {
         return pokemonLocalDataSource.getFavoritePokemons().map {
             DataMapper.mapPokemonEntitiesToDomains(it)
-        }.flowOn(Dispatchers.IO)
+        }
     }
 
     override suspend fun updatePokemonFavorite(pokemonDetail: PokemonDetail, isFavorite: Boolean) {
