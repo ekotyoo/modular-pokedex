d3cd6c893187e4244e1c2cad45aaf069fd7bc692
ekotyoo <ekeprasetyo@gmail.com>
2022-07-22 00:39:43 +0700

    Add certificate pinning on OkHttp

--- core/src/main/java/com/ekotyoo/core/di/NetworkModule.kt
+++ core/src/main/java/com/ekotyoo/core/di/NetworkModule.kt
@@ -5,6 +5,7 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.CertificatePinner
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
@@ -16,11 +17,21 @@ import java.util.concurrent.TimeUnit
 class NetworkModule {
 
     @Provides
-    fun provideOkHttpClient(): OkHttpClient = OkHttpClient.Builder()
-        .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
-        .connectTimeout(15, TimeUnit.SECONDS)
-        .readTimeout(15, TimeUnit.SECONDS)
-        .build()
+    fun provideOkHttpClient(): OkHttpClient {
+        val hostname = "pokeapi.co"
+        val certificatePinner = CertificatePinner.Builder()
+            .add(hostname, "sha256/D7nVX3nnTE0NXEmTWhim6nTMVFFIJWRuXenQGaeSRIw=")
+            .add(hostname, "sha256/FEzVOUp4dF3gI0ZVPRJhFbSJVXR+uQmMH65xhs1glH4=")
+            .add(hostname, "sha256/Y9mvm0exBk1JoQ57f9Vm28jKo5lFm/woKcVxrYxu80o=")
+            .build()
+
+        return OkHttpClient.Builder()
+            .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+            .connectTimeout(15, TimeUnit.SECONDS)
+            .readTimeout(15, TimeUnit.SECONDS)
+            .certificatePinner(certificatePinner)
+            .build()
+    }
 
     @Provides
     fun providePokemonApiService(client: OkHttpClient): PokemonApiService {
